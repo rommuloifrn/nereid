9d6e3511c89948c8db5b79049d26ee82005bc698
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-07-04 19:10:37 -0300

    start Angular project, configure Tailwind & create basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainviewComponent } from './components/mainview/mainview.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TopbarComponent,
+    SidebarComponent,
+    MainviewComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/mainview/mainview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainview',
+  standalone: true,
+  imports: [],
+  templateUrl: './mainview.component.html',
+  styleUrl: './mainview.component.css'
+})
+export class MainviewComponent {
+
+}
