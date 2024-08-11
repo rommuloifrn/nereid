e1fd08a947edf4c69744baf2624808fe1d84462a
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-08-11 19:15:46 -0300

    style: remove clutter from localStorage testing

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Diagram } from '../../diagram';
 import { DiagramService } from '../../services/diagram/diagram.service';
 import { CreateClassBtnComponent } from './create-class-btn/create-class-btn.component';
 
@@ -16,9 +15,4 @@ import { CreateClassBtnComponent } from './create-class-btn/create-class-btn.com
 })
 export class SidebarComponent {
   diagramService: DiagramService = inject(DiagramService)
-  diagram: Diagram = this.diagramService.currentDiagram;
-
-  getDia() {
-    this.diagram = this.diagramService.currentDiagram
-  }
 }
--- src/app/services/diagram/diagram.service.ts
+++ src/app/services/diagram/diagram.service.ts
@@ -1,61 +1,21 @@
 import { afterRender, Injectable } from '@angular/core';
-import { Class } from "../../class";
 import { Diagram } from '../../diagram';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiagramService {
-  classList:Class[] = [
-    {
-      "title":"Animal",
-      "attributes":[
-        {
-          "attributeName":"ximbas"
-        }
-      ]
-    },
-    {
-      "title":"Hunter",
-      "attributes":[]
-    },
-    {
-      "title":"Plant",
-      "attributes":[]
-    }
-  ]
 
   currentDiagram: Diagram = {"classes":[]}//{"classes":this.classList}
-  classes: Class[] = []
 
   create() {
     this.currentDiagram.classes.push({"title":"ximbas","attributes":[]});
     this.saveDiagram();
-
-    this.classes.push({"title":"ximbas","attributes":[]})
-    this.saveClasses();
   }
 
   saveDiagram() {
     let stringDiagram = JSON.stringify(this.currentDiagram);
     localStorage.setItem("diagram", stringDiagram);
-  }
-
-  saveClasses() {
-    let stringClasses = JSON.stringify(this.classes);
-    localStorage.setItem("classes", stringClasses);
-  }
-
-  get() {
-    let diagram: string | null = "";
-    if (localStorage.getItem("diagram") != null) diagram = localStorage.getItem("diagram");
-    if (diagram == null) diagram = "";
-    this.currentDiagram = JSON.parse(diagram);
-    console.log("lgoaassssss");
-    console.log("diagram:");
-    
-    console.log(this.currentDiagram);
-    return this.currentDiagram;
   }
 
   constructor() {
@@ -64,16 +24,6 @@ export class DiagramService {
       if (localStorage.getItem("diagram") != null) diagram = localStorage.getItem("diagram");
       if (diagram == null) diagram = "";
       this.currentDiagram = JSON.parse(diagram);
-      console.log("lgoaassssss");
-      console.log("diagram:");
-      
-      console.log(this.currentDiagram);
-
-      let classes: string | null = "";
-      if (localStorage.getItem("classes") != null) classes = localStorage.getItem("classes");
-      if (classes == null) classes = "";
-      this.classes = JSON.parse(classes);
-      
     })
   }
 
