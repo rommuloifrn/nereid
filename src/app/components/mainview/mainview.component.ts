365b5664b390e04da185b4096fe5948ad87d270c
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-08-19 10:08:25 -0300

    wip: mermaid rendering (is not working on changes)

--- src/app/components/mainview/mainview.component.ts
+++ src/app/components/mainview/mainview.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, ElementRef, inject, OnChanges, ViewChild } from '@angular/core';
+import mermaid from 'mermaid';
+import { DiagramService } from '../../services/diagram/diagram.service';
+import { Diagram } from 'mermaid/dist/Diagram';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mainview',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './mainview.component.html',
   styleUrl: './mainview.component.css'
 })
-export class MainviewComponent { //https://stackoverflow.com/questions/60156296/problems-with-mermaid-integration-in-angular
+export class MainviewComponent{ //https://stackoverflow.com/questions/60156296/problems-with-mermaid-integration-in-angular
 
+  diagramService: DiagramService = inject(DiagramService)
+  classes = this.diagramService.currentDiagram.classes
+
+  constructor(){
+    afterNextRender(()=>{
+      read: () => {
+        void mermaid.init()
+      }
+    })
+  }
 }
