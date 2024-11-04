13e4fa370607464d8e33065a15d84d096cfd137d
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-11-04 13:51:54 -0300

    refactor: mainview functions that render diagram at start

--- src/app/components/mainview/mainview.component.ts
+++ src/app/components/mainview/mainview.component.ts
@@ -15,30 +15,12 @@ import { DiagramService } from '../../services/diagram/diagram.service';
 export class MainviewComponent {
 
   ds: DiagramService = inject(DiagramService)
+  classes: Class[] = this.ds.currentDiagram.classes
 
-  async openingTheShop() {
-    await this.ds.loadDiagramFromStorage();
+  constructor(){    
     setTimeout(() => {
       this.ds.updateDiagramRender(); 
-    });
-  }
-
-  //@Input({required: true}) classes!: Class[];
-  classes: Class[] = this.ds.currentDiagram.classes
-  constructor(){
-    afterNextRender(()=>{ // essa merda é necessaria mesmo se nao tiver nada dentro... https://stackoverflow.com/questions/60156296/problems-with-mermaid-integration-in-angular
-      read: () => {
-        //void mermaid.init()
-        console.log("ximbas");
-        
-      }
-
-      setTimeout(() => {
-        this.openingTheShop()
-      }, 100);
-      console.log(this.ds.currentDiagram);
-      
-    })
+    }, 100); // isso aqui é basicamente pra dar tempo até o diagrama ser carregado do localStorage. Não é definitivo.
   }
   
 }
