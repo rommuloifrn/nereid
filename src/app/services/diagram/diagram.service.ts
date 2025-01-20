92fce64ce7368b93974ba110e2764fac8d0989a5
rommuloifrn <roomulo.cavalkant@outlook.com>
2025-01-19 21:55:56 -0300

    feat: relationship multiplicity (right and left symbols)

--- src/app/services/diagram/diagram.service.ts
+++ src/app/services/diagram/diagram.service.ts
@@ -38,14 +38,13 @@ export class DiagramService {
     return false;
   }
 
-  AddRelationship(leftPartner: Class, rightPartner: Class) {
-    let r: Relationship = new Relationship(this.lastRelationshipIdUsed++, leftPartner, "", rightPartner, "");
+  AddRelationship(leftPartner: Class, rightPartner: Class, leftSymbol: string, rightSymbol: string) {
+    let r: Relationship = new Relationship(this.lastRelationshipIdUsed++, leftPartner, leftSymbol, rightPartner, rightSymbol);
     this.currentDiagram.relationships.push(
       r
     );
     this.saveDiagram();
     this.updateDiagramRender();
-    console.log(r.id);
     
   }
 
@@ -128,7 +127,7 @@ export class DiagramService {
     let body: string = "classDiagram\ndirection DT\n";
 
     for (var r of d.relationships) {
-      body = body.concat(r.leftPartner.title, " ", " -- ", " ", r.rightPartner.title, "\n");
+      body = body.concat(r.leftPartner.title, ("\"").concat(r.leftSymbol).concat("\""), " -- ", ("\"").concat(r.rightSymbol).concat("\""), r.rightPartner.title, "\n");
     }
 
     for (var c of d.classes) {
